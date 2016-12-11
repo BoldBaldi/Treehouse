1fbe2a6dd15b50fe9d35388e952f07bbd27276f4
Dirk Walzel <dirk.walzel@hotmail.de>
2016-12-11 15:50:26 +0100

    Added Newton.JSON and Player Class from JSON

---
+++ Streams1/Streams1/GameResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Streams1
+{
+    public class GameResult
+    {
+        public DateTime GameDate { get; set; }
+        public string TeamName { get; set; }
+        public HomeOrAway HomeOrAway { get; set; }
+        public int Goals { get; set; }
+        public int GoalAttemps { get; set; }
+        public int ShotsOnGoal { get; set; }
+        public int ShotsOffGoal { get; set; }
+        public double PossessionPercent { get; set; }
+        public double ConversionRate
+        {
+            get
+            {
+                return (double)Goals / (double)GoalAttemps;
+            }
+        }
+    }
+}
+
+public enum HomeOrAway
+{
+    Home,
+    Away
+}
--- Streams1/Streams1/Program.cs
+++ Streams1/Streams1/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace SoccerStats
+namespace Streams1
 {
     class Program
     {
@@ -14,14 +14,11 @@ namespace SoccerStats
             string currentDirectory = Directory.GetCurrentDirectory();
             DirectoryInfo directory = new DirectoryInfo(currentDirectory);
             var fileName = Path.Combine(directory.FullName, "SoccerGameResults.csv");
-            var fileContents = ReadFile(fileName);
-            string[] fileLines = fileContents.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var line in fileLines)
-            {
-                Console.WriteLine(line);
-            }
-
+            var fileContents = ReadSoccerResults(fileName);
+            
         }
+
+        
 
         public static string ReadFile(string fileName)
         {
@@ -30,5 +27,69 @@ namespace SoccerStats
                 return reader.ReadToEnd();
             }
         }
+
+        public static List<GameResult> ReadSoccerResults(string fileName)
+        {
+            var soccerResults = new List<GameResult>();
+            using (var reader = new StreamReader(fileName))
+            {
+                string line = "";
+                reader.ReadLine();
+                while ((line = reader.ReadLine())!=null)
+                {
+                    var gameResult = new GameResult();
+                    string[] values = line.Split(',');
+                    DateTime gameDate;
+                    System.Globalization.CultureInfo culture;
+                    System.Globalization.DateTimeStyles styles;
+                    System.Globalization.NumberStyles style;
+
+                    // Parse a date and time with no styles.
+
+                    culture = System.Globalization.CultureInfo.CreateSpecificCulture("en-US");
+                    styles = System.Globalization.DateTimeStyles.None;
+                    
+                    if (DateTime.TryParse(values[0],culture,styles, out gameDate))
+                    {
+                        gameResult.GameDate = gameDate;
+                    }
+                    gameResult.TeamName = values[1];
+                    HomeOrAway homeOrAway;
+                    if(Enum.TryParse(values[2], out homeOrAway))
+                    {
+                        gameResult.HomeOrAway = homeOrAway;
+                    }
+                    int parseInt;
+                    if(int.TryParse(values[3],out parseInt))
+                    {
+                        gameResult.Goals = parseInt;
+                    }
+                    if (int.TryParse(values[4], out parseInt))
+                    {
+                        gameResult.GoalAttemps = parseInt;
+                    }
+                    if (int.TryParse(values[5], out parseInt))
+                    {
+                        gameResult.ShotsOnGoal = parseInt;
+                    }
+                    if (int.TryParse(values[6], out parseInt))
+                    {
+                        gameResult.ShotsOffGoal = parseInt;
+                    }
+                   double parseDouble;
+                   style = System.Globalization.NumberStyles.Number | System.Globalization.NumberStyles.AllowCurrencySymbol;
+
+                    if (double.TryParse(values[7], style, culture, out parseDouble))
+                    {
+                        gameResult.PossessionPercent= parseDouble;
+                    }
+                    
+                    soccerResults.Add(gameResult);
+                }
+            }
+            return soccerResults;
+
+        }
+        
     }
 }
