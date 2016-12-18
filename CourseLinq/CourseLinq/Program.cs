0f50ea6c69f7142a47e9b918580cd5700b603c22
Dirk Walzel <dirk.walzel@hotmail.de>
2016-12-18 13:15:49 +0100

    Conversate and Greeting (Action and Fuc)

---
+++ CourseLinq/CourseLinq/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseLinq
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            Action<string> sayGreeting;
+
+            Func<string, string> conversate = delegate (string message)
+            {
+                Console.WriteLine(message);
+                return Console.ReadLine();
+            };
+            
+            string input = conversate("What's your name?");
+
+            sayGreeting = delegate (string greeting)
+            {
+                Console.WriteLine(string.Format(greeting, input));
+            };
+                        
+            sayGreeting("Hello, {0}!");
+            sayGreeting("I think {0} is a beautiful name, {0}. Mine is Josh.");
+            conversate("Are you doing well?");
+            sayGreeting("Later, {0}!");
+            Console.ReadLine();
+        }
+    }
+}
