511d6470948ca5f5eb5711b110605d1e2c4ba0c3
Dirk Walzel <dirk.walzel@hotmail.de>
2016-12-18 18:29:06 +0100

    Coming Back

--- CourseLinq/CourseLinq/Program.cs
+++ CourseLinq/CourseLinq/Program.cs
@@ -4,28 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
+
 namespace CourseLinq
 {
     class Program
     {
-       
+        
+
         static void Main(string[] args)
         {
+
             Action<string> sayGreeting;
 
-            Func<string, string> conversate = delegate (string message)
+            Func<string, string> conversate = (message) =>
             {
                 Console.WriteLine(message);
                 return Console.ReadLine();
             };
-            
+
             string input = conversate("What's your name?");
 
-            sayGreeting = delegate (string greeting)
+            sayGreeting = (greeting) =>
             {
                 Console.WriteLine(string.Format(greeting, input));
             };
-                        
+
             sayGreeting("Hello, {0}!");
             sayGreeting("I think {0} is a beautiful name, {0}. Mine is Josh.");
             conversate("Are you doing well?");
